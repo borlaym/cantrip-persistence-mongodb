98507cd09aa905e078c1e5ff518bfae0cacfe10a
Márton Borlay <marton.borlay@kriek.hu>
2015-02-18 17:58:29 +0100

    Get now works

--- index.js
+++ index.js
@@ -2,213 +2,150 @@ var fs = require("fs");
 _ = require("lodash");
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
+var ObjectID = mongo.ObjectID;
 
-var queue = [];
-var lock = false;
-
-function added(cantrip) {
-	if (queue.length > 0 && !lock) {
-		var data = queue.shift();
-		lock = true;
-		cantrip.data.update({
-			path: data.path
-		},
-			data, {
-			upsert: true,
-			safe: true
-		}, function(err, docs) {
-			lock = false;
-			added(cantrip);
-		});
-	}
-}
-
-module.exports = {
-	setupPersistence: function(callback) {
-		var self = this;
-		if (!this.options.mongodb) this.options.mongodb = {
+module.exports =  function(options) {
+	options = _.extend({
+		mongodb: {
 			ip: "localhost",
 			port: 27017,
 			database: "cantrip"
-		};
-		MongoClient.connect('mongodb://'+this.options.mongodb.ip+':'+this.options.mongodb.port+'/' + this.options.mongodb.database, function(err, db) {
-			if (err) throw err;
-			self.data = db.collection(self.options.namespace);
-			self.dataStore.data = self.data;
-			self.dataStore.data.update({path: "/_contents"}, {path: "/_contents", value: "object"}, {upsert:true, safe:true}, function() {
-				callback();
-			});
-		});
-	},
-	syncData: function() {},
-	dataStore: {
-		deleteNodes: function(path, callback) {
-			this.data.remove({
-				path: new RegExp(path)
-			}, function(err, res) {
-				callback && callback(err, res);
-			});
-		},
-		setNode: function(path, value, callback) {
-			queue.push({path: path, value: value});
-			added(this);
-		},
-		get: function(path, callback) {
-			this.data.find({
-				path: new RegExp(path)
-			}).sort({path: 1}, function(err, res) {
+		}
+	}, options);
 
-				if (err) {
-					callback(err, null);
-					return;
-				}
+	return function(callback) {
 
-				res.toArray(function(err, array) {
+		MongoClient.connect('mongodb://'+options.mongodb.ip+':'+options.mongodb.port+'/' + options.mongodb.database, function(err, db) {
 
-					if (err) {
-						callback(err, null);
-						return;
-					}
-					var result = null; //This will hold the resulting object. If there were no documents found, it stays on null
-					if (array.length === 0) {
-						if (path === "/_contents/") callback(null, {});
-						else callback({error: "Requested node doesn't exist."}, null);
-						return;
-					}
-					//Handle single ended queries, when all we return is a single value, an empty object or array
-					if (array.length === 1 && path !== "/_contents/") {
-						if (array[0].value === "object") result = {};
-						else if (array[0].value === "array") result = [];
-						else result = {
-							value: array[0].value
-						}; //return a simple value: value object when the end result would be of a basic type
-						callback(null, result);
-						return;
+			if (err) return callback && typeof callback === 'function' && callback(err);
+			
+			/**
+			 * Return the datastore 
+			 */
+			return callback && typeof callback === 'function' && callback(null, {
+
+				get: function(path, callback) {
+					path = _.filter(path.split("/"), function(string) {
+						return string !== "";
+					});
+
+					if (path.length === 0) {
+						return callback && typeof callback === 'function' && callback({
+							error: "Sorry, can't show you the whole database right now"
+						}, node);
+					
 					}
 
-					//If there are more than one documents found, let's build the resulting object up from a basic object
-					result = {};
-					//Dig into the results. We loop through the nodes (objects) returned by the query
-					for (var i = 0; i < array.length; i++) {
-						var node = array[i]; //This is the current node in our json tree
-						if (node.path.replace(path, "").substr(1) === "") {
-							if (node.value === "array") result = []; //If the requested root node is an array, replace the base result variable with an empty array
-							continue; //This is basically the first result. When we encounter it, we continue
-						}
-						var toReplace = path === "/" ? "" : path;
-						if (path === "/_contents/") toReplace = "_contents/";
-						var members = node.path.replace(toReplace, "").substr(1).split("/"); //We omit the request path from the node's path attribute to get a relative reference, also strip the first / character
-						var previousNode = null; //This is a pointer to the previous node
-						var pointer = result; //This pointer will walk through our json object, searching for the place where the current node resides, so it can add a value to it
-						//Loop through the nodes. foo/bar will become result.foo.bar
-						for (var j = 0; j < members.length; j++) {
-							previousNode = pointer; //This is a pointer to the previously checked node. Used for determining whether we're inside an array or an object
-							if (j === members.length - 1) { //At the end of the pointer's walk, we add a value based on the node's value property
-								if (node.value === "object") {
-									if (_.isArray(previousNode)) previousNode.push({
-										_id: members[j]
-									});
-									else pointer[members[j]] = {};
-								} else if (node.value === "array") {
-									if (_.isArray(previousNode)) previousNode.push([]);
-									else pointer[members[j]] = [];
+					var collection = db.collection(path.shift());
+					if (path.length === 0) {
+						collection.find({}).toArray(function(err, docs) {
+							_.map(docs, function(doc) {
+								doc._id = doc._id.toString();
+							})
+							return callback && typeof callback === 'function' && callback(null, docs);
+						});
+					} else {
+
+						var _id = ObjectID(path.shift());
+						collection.findOne( { _id: _id }, function(err, doc) {
+							console.log(err, doc);
+							doc._id = doc._id.toString();
+
+							if (path.length === 0) return callback && typeof callback === 'function' && callback(null, doc);
+
+							var node = doc;
+							//Loop through the data by the given paths
+							for (var i = 0; i < path.length; i++) {
+								var temp = node[path[i]];
+								//If we found the given key, assign the node object to its value
+								if (temp !== undefined) {
+									node = node[path[i]];
+									//If the given key doesn't exist, try the _id
 								} else {
-									if (_.isArray(previousNode)) previousNode.push(node.value);
-									else {
-											pointer[members[j]] = node.value;
+									temp = _.find(node, function(obj) {
+										return obj._id === path[i];
+									});
+									//If it's not undefined, then assign it as the value
+									if (temp !== undefined) {
+										node = temp;
+									} else {
+										return callback && typeof callback === 'function' && callback({
+											status: 404,
+											error: "Requested node doesn't exist."
+										}, null);
 									}
 								}
-							} else {
-								if (_.isArray(previousNode)) {
-									//If the parent node was an array, we can't just use parent.current syntax, we need to find a member of the array by id (or index, in the case of simple arrays instead of collections)
-									pointer = _.find(previousNode, function(obj) {
-										return obj._id === members[j];
-									});
-								} else {
-									//Set the pointer as parent.current
-									pointer = pointer[members[j]];
-								}
 							}
-						}
+
+							return callback && typeof callback === 'function' && callback(null, node);
+						});	
 					}
-					callback(null, result);
-				});
-			});
-		},
-		set: function(path, data, callback) {
-			var self = this;
-			this.data.find({path: path}, function(err, res) {
-				res.toArray(function(err, target) {
-					//The function that goes through the data object to insert actual documents to the database
-					var insert = function(obj, pointer) {
-						for (var key in obj) {
-							if (!_.isObject(obj[key])) {
-								if (pointer === "/_contents/") pointer = "/_contents"; //Fix when we try to set the root "/"
-								if (pointer === "/") pointer = ""; //Fix when we try to set the root "/"
-								self.setNode(pointer + "/" + key, obj[key]);
-								self.deleteNodes(pointer + "/" + key + "\/"); //Delete all previous values this object had
-							} else {
-								if (pointer === "/_contents/") pointer = "/_contents"; //Fix when we try to set the root "/"
-								if (pointer === "/") pointer = ""; //Fix when we try to set the root "/"
-								var keyToContinue = key;
-								if (obj[key]._id) {
-									keyToContinue = obj[key]._id;
-								}
-								if (_.isArray(obj[key])) self.setNode(pointer + "/" + key, "array");
-								else {
-									self.setNode(pointer + "/" + keyToContinue, "object");
-								}
-								insert(obj[key], pointer + "/" + keyToContinue);
-							}
-						}
-					};
-					//Target is the root or an object
-					if (target.length === 0 || target[0].value === "object") {
-						//MERGE
-						insert(data, path);
-						callback(null, null);
-						
-					//Target is an array
-					} else if (target[0].value === "array") {
-						//PUSH behavior
-						if (!_.isObject(data) || !data._id) {
-							//Get the index! We need the maximum index of the object
-							self.data.find({path: new RegExp(path)}, function(err, res) {
-								res.toArray(function(err, elements) {
-									var index = _.reduce(elements, function(memo, element) {
-										var index = Number(element.path.replace(path, "").split("/")[0]);
-										if (index > memo) memo = index;
-									}, -1) + 1;
-									self.setNode(path + "/" + index, "object");
-									insert(data, path + "/" + index);
-									callback(null, null);
-								});
-							});
-							
-						} else {
-							//MERGE behavior (not really, but at least we can use the _id property as an index)
-							self.setNode(path + "/" + data._id, "object");
-							insert(data, path + "/" + data._id);
-							callback(null, null);
-						}
-					//Target is a basic value
-					} else {
-						callback({error: "Can't set value of a basic value."}, null);
-					}
+
 					
-				});
+				},
+				set: function(path, data, patch, callback) {
+					var self = this;
+					this.get(path, function(err, target) {
+						if (_.isArray(target)) {
+							//POST
+							target.push(data);
+							callback && typeof callback === 'function' && callback(null);
+							syncData();
+						} else if (_.isObject(target)) {
+							//PATCH
+							if (patch) {
+								target = _.merge(target, data);
+								callback && typeof callback === 'function' && callback(null);
+								syncData();
+							} else {
+								//PUT
+								self.parent(path, function(err, parent) {
+									var toPut = _.last(path.split("/"));
+									if (toPut === "") {
+										_data = data;
+									} else {
+										parent[toPut] = data;
+									}
+									callback && typeof callback === 'function' && callback(null);
+									syncData();
+								});
+							}
+						} else {
+							self.parent(path, function(err, parent) {
+								parent[_.last(path.split("/"))] = data;
+								callback && typeof callback === 'function' && callback(null);
+								syncData();
+							});
+						}
+					});
+				},
+				delete: function(path, callback) {
+					var index = _.last(path.split("/"));
+					this.parent(path, function(err, parent) {
+						if (_.isArray(parent)) {
+							if (_.isNumber(Number(index)) && !_.isNaN(Number(index))) {
+								parent.splice(index, 1);
+								//If it's a hash (string), we find the target object, get it's index and remove it from the array that way
+							} else {
+								var obj = _.find(parent, function(obj) {
+									return obj._id === index;
+								});
+								parent.splice(_.indexOf(parent, obj), 1);
+							}
+						} else if (_.isObject(parent)) {
+							delete parent[index];
+						}
+						callback && typeof callback === 'function' && callback(null);
+						syncData();
+					});
+				},
+				parent: function(path, callback) {
+					this.get(path.split("/").slice(0, -1).join("/"), function(err, parent) {
+						callback && typeof callback === 'function' && callback(err, parent);
+					});
+				}
 			});
-		},
-		delete: function(path, callback) {
-			this.deleteNodes(path, function() {
-				callback();
-			});
-		},
-		parent: function(path, callback) {
-			var parentPath = path.split("/").slice(0, -1).join("/");
-			this.get(parentPath, function(err, res) {
-				callback(err, res);
-			});
-		}
+		});
+
 	}
 }
--- server.js
+++ server.js
@@ -1,7 +1,38 @@
-var Cantrip = require("Cantrip");
-var mongodb = require("./index.js");
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser')
+var mongoPersistence = require("./index");
 
-Cantrip.options.persistence = mongodb;
-Cantrip.options.port = 3000;
+require("Cantrip")({
+	persistence: mongoPersistence()
+}, function(err, cantrip) {
+	if (err) console.log(err);
+	var app = express();
+	app.use(bodyParser.json());
+	app.use(function(err, req, res, next) {
+		return next({
+			status: 400,
+			error: "Invalid JSON supplied in request body."
+		});
+	});
 
-Cantrip.start();
+	app.use(bodyParser.urlencoded());
+	app.use(cors());
+
+	app.use(cantrip);
+
+	app.use(function(req, res, next) {
+		res.send(res.body);
+	});
+
+	app.use(function(err, req, res, next) {
+		if (err.status) res.status(err.status);
+		res.send({
+			error: err.error
+		});
+	});
+
+	app.listen(3000);
+
+});
+
